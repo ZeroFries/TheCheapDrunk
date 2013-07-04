11bab2a0609ecd5a652a098b18a9d02e1caebab4
ZeroFries <sgarvagh@uoguelph.ca>
2013-07-04 13:25:21 -0400

    finished basic product list

--- app.rb
+++ app.rb
@@ -1,14 +1,36 @@
 require 'sinatra'
 
-# index
-def create_array(cat)
+def convert_cents_to_dollars(price)
+	price = price.to_s
+	if price.size == 3
+		price[0,1] + "." + price[1,3]
+	elsif price.size == 4
+		price[0,2] + "." + price[2,4]
+	end
+end
+
+def convert_to_percentage(alcohol)
+	alcohol = alcohol.to_s
+	if alcohol.size == 3
+		alcohol[0,1] + "." + alcohol[1,3]
+	elsif alcohol.size == 4
+		alcohol[0,2] + "." + alcohol[2,4]
+	end
+	(alcohol.to_f / 100).to_s
+end
+
+def create_array(cat) # creates an array of beverages (a hash of properties)
 	file = File.open "Beverages.txt", "r"
 	counter = 0
 	arr = []
 	file.each_line do |line|
 		if counter > 0
 			contents = line.split("* ")
-			hash = { name: contents[0], price: contents[1], thumb: contents[2], container: contents[3], alcohol: contents[4], alcoholPrice: contents[5] }
+			hash = { name: contents[0], 
+				price: convert_cents_to_dollars(contents[1]),
+				thumb: contents[2], container: contents[3],
+				alcohol: convert_to_percentage(contents[4]),
+				alcoholPrice: convert_cents_to_dollars(contents[5]) }
 			arr << hash
 			counter -= 1
 		end
